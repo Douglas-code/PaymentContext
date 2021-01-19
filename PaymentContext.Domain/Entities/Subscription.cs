4cddbaa01d6778644919a32748ff5724e8d116ca
Douglas Rodrigues <douglas.pereira@basis.com.br>
2021-01-18 21:27:05 -0300

    first commit

---
+++ PaymentContext.Domain/Entities/Subscription.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PaymentContext.Domain.Entities
+{
+    public class Subscription
+    {
+        public DateTime CreateDate { get; set; }
+
+        public DateTime LastUpdateDate { get; set; }
+
+        public DateTime? ExpireDate { get; set; }
+
+        public bool Active { get; set; }
+
+        List<Payment> Payments { get; set; }
+    }
+}
